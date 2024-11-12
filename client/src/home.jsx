deb6b8904f30e4283895cc6bcb86551bd065af4b
Mai <ttmai@my.loyno.edu>
2024-11-12 17:17:28 -0500

    add background img and worked on CSS

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,13 +1,21 @@
 import NavBar from "./Navbar";
+import './CSS/Home.css';
 
 function Home() {
-
-
     return (
         <>
-        <NavBar/>
+            <body className="body">
+                <h1> Mai Portfolio </h1>
+                <NavBar />
+                <div className="photo-container">
+                    <img
+                        src='/Mai.jpg'
+                        alt="A femme-presenting Asian-American person, wearing hoop earrings with bright pink hair smiling against the background of a forest"
+                    />
+                </div>
+            </body>
         </>
-    )
+    );
 }
 
 export default Home;
