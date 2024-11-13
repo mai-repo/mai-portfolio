e7da4b9215e930f9f147595804c5a56d1740534e
Mai <ttmai@my.loyno.edu>
2024-11-12 19:37:58 -0500

    editing CSS

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -7,11 +7,16 @@ function Home() {
             <body className="body">
                 <h1> Mai Portfolio </h1>
                 <NavBar />
-                <div className="photo-container">
-                    <img
-                        src='/Mai.jpg'
-                        alt="A femme-presenting Asian-American person, wearing hoop earrings with bright pink hair smiling against the background of a forest"
-                    />
+                <div className="third-row">
+                    <div className="photo-container">
+                        <img
+                            src='/Mai.jpg'
+                            alt="A femme-presenting Asian-American person, wearing hoop earrings with bright pink hair smiling against the background of a forest"
+                        />
+                    </div>
+                    <div className="text">
+                        
+                    </div>
                 </div>
             </body>
         </>
